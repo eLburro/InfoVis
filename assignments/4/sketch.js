9d5cac7791419898d0604417cc3b7775ae58e403
eL burro <ramon.burri@gmail.com>
2017-01-05 15:56:22 +0100

    applied feedback

--- assignments/4/sketch.js
+++ assignments/4/sketch.js
@@ -1,5 +1,8 @@
 var COL_HEADER_AUTHOR_NAMES = "Deduped.author.names";
 var COL_HEADER_AUTHOR_KEYWORDS = "Author.Keywords";
+var COL_HEADER_YEAR = "Year";
+
+var MAX_PUBLISHING_YEAR = 2015;
 
 var canvasWidth = 1000;
 var canvasHeight = 1000;
@@ -8,18 +11,18 @@ var table;
 var author2Topic = {};
 
 // drawing
-var CENTER_X = 300;
-var CENTER_Y = 300;
+var CENTER_X = 400;
+var CENTER_Y = 400;
 var RAD = 80;
 
 // this would be available over dropdown or search field
-var TEST_AUTHOR_NAME_1 = "Garth, C";
+var TEST_AUTHOR_NAME_1 = "Bostock, M.";
 var TEST_AUTHOR_NAME_2 = "Gao, J.";
 var TEST_AUTHOR_NAME_3 = "van Wijk, J.J.";
 var TEST_KEYWORD = "object modeling";
 
 function preload() {
-    table = loadTable("../../data/author2topic.csv", "csv", "header");
+    table = loadTable("../../data/author2topicAndYear.csv", "csv", "header");
 }
 
 function setup() {
@@ -32,26 +35,31 @@ function setup() {
         var row = table.getRow(i);
         var rowAuthor = row.get(COL_HEADER_AUTHOR_NAMES);
         var rowKeyword = row.get(COL_HEADER_AUTHOR_KEYWORDS);
+        var rowYear = row.get(COL_HEADER_YEAR);
 
         if (author2Topic[rowAuthor] == undefined) {
             author2Topic[rowAuthor] = {};
-            author2Topic[rowAuthor][rowKeyword] = 1;
+            author2Topic[rowAuthor][rowKeyword] = {'count': 1, 'year': rowYear};
 
         } else {
            if (author2Topic[rowAuthor][rowKeyword] == undefined) {
-               author2Topic[rowAuthor][rowKeyword] = 1;
+               author2Topic[rowAuthor][rowKeyword] = {'count': 1, 'year': rowYear};
 
            } else {
-               author2Topic[rowAuthor][rowKeyword] += 1;
+               author2Topic[rowAuthor][rowKeyword].count += 1;
+
+               if (author2Topic[rowAuthor][rowKeyword].year < rowYear) {
+                   author2Topic[rowAuthor][rowKeyword].year = rowYear;
+               }
            }
         }
     }
 
-    //console.log(author2Topic);
+    console.log(author2Topic);
 }
 
 function draw() {
-    var authorToDisplay = author2Topic[TEST_AUTHOR_NAME_2];
+    var authorToDisplay = author2Topic[TEST_AUTHOR_NAME_1];
     var topicCount = Object.keys(authorToDisplay).length;
     var alpha = 360 / topicCount;
 
@@ -60,13 +68,13 @@ function draw() {
 
     // display author name
     textSize(32);
-    text("Author: " + TEST_AUTHOR_NAME_2, 0, 32);
+    text("Author: " + TEST_AUTHOR_NAME_1, 0, 32);
 
     // draw helper circles
     drawHelperCircles(maxLevels);
 
-    // reset colors
-    fill(0,0,0);
+    // reset colors & sizes
+    reset();
 
     var bX = 0
     var bY = 0;
@@ -76,7 +84,8 @@ function draw() {
 
     // loop over topics of author
     for (var topic in authorToDisplay) {
-        var count = authorToDisplay[topic];
+        var count = authorToDisplay[topic].count;
+        var year = authorToDisplay[topic].year;
 
         // calculate point c of the triangle
         bX = RAD * count * Math.sin(toRadians(angle-alpha)) + CENTER_X;
@@ -85,8 +94,20 @@ function draw() {
         cX = RAD * count * Math.sin(toRadians(angle)) + CENTER_X;
         cY = RAD * count * Math.cos(toRadians(angle)) + CENTER_Y;
 
+        // add fill saturation based on last publishing year
+        fill(getSaturation(year));
+
         // draw triangle with center point, b & c as corners
         triangle(CENTER_X, CENTER_Y, bX, bY, cX, cY);
+
+        // add label to the end of the triangle
+        var midX = (bX + cX) / 2;
+        var midY = (bY + cY) / 2;
+        var lineLen = getLineLength(count, maxLevels);
+        var endPoint = getLineSegmentPoint(CENTER_X, CENTER_Y, midX, midY, lineLen);
+        reset();
+        line(midX, midY, endPoint.x, endPoint.y);
+        text(topic, endPoint.x, endPoint.y);
 
         bX = cX;
         bY = cY;
@@ -108,14 +129,50 @@ function getMaxTopicCount(topics) {
     var max = 1;
 
     for (var topic in topics) {
-        if (max < topics[topic]) {
-            max = topics[topic];
+        if (max < topics[topic].count) {
+            max = topics[topic].count;
         }
     }
 
     return max;
 }
 
-function toRadians (angle) {
+function toRadians(angle) {
     return angle * (Math.PI / 180);
+}
+
+/**
+ * Saturation gets calculated based on the year of last publishing.
+ * Year range is from 1990 to 2015 which is a difference from 0 to 25.
+ *
+ * @param year The last publishing year
+ */
+function getSaturation(year) {
+    var diff = MAX_PUBLISHING_YEAR - year;
+
+    if (diff < 5) {
+        return 0;
+    } else if (diff < 15) {
+        return 100
+    } else {
+        return 200
+    }
+}
+
+function getLineSegmentPoint(x1, y1, x2, y2, len2add) {
+    var lenAB = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    var newX = x2 + (x2 - x1) / lenAB * len2add;
+    var newY = y2 + (y2 - y1) / lenAB * len2add;
+
+    return {'x': newX, 'y': newY};
+}
+
+function getLineLength(count, maxCount) {
+   var multiplier = (maxCount + 1) - count;
+   return multiplier * RAD;
+}
+
+function reset() {
+    fill(0,0,0);
+    textSize(12);
 }
